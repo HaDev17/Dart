f55ff4fd8db7f99eabe6b6e183f591be2aacb19d
HaDev17 <104528704+HaDev17@users.noreply.github.com>
2024-07-29 18:18:42 +0100

    dart

---
+++ syntaxBasic/helloword.dart
@@ -0,0 +1,8 @@
+// To print a simple message to the console, you can use the print() function.
+
+void main() {
+  print('Hello, world!');
+}
+
+// The main() function is the entry point of a Dart program.
+// void is the return type of the main() function, and it means that the function does not return any value.
---
+++ syntaxBasic/variables.dart
@@ -0,0 +1,24 @@
+// Variables in dart are declared using the var keyword.
+var name = "Habib";
+var age = 25;
+var height = 5.8;
+
+// The type of the variable is automatically inferred by the compiler.
+// The type of the variable can also be explicitly mentioned.
+String name1 = "Habib";
+int age1 = 25;
+double height1 = 5.8;
+
+// There are two types of variables in Dart: mutable and immutable.
+// Mutable variables can be changed after they are declared.
+// Immutable variables cannot be changed after they are declared.
+
+// The final keyword is used to declare a variable whose value cannot be changed.
+final String name2 = "Habib";
+// we can also use the const keyword to declare a variable whose value cannot be changed.
+const String name3 = "Habib";
+
+
+// The Object type can store any type of value. 
+// It is the parent class of all the classes in Dart.
+
